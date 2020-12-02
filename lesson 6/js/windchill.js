a1a71b05d5205c55a11285d5d3b6b6370b4fc55f
Spart0n <avizza88@gmail.com>
2020-12-02 12:46:13 -0800

    page add

---
+++ lesson 6/js/windchill.js
@@ -0,0 +1,12 @@
+function doInputOutput () 
+{
+var temp , a = parseFloat(document.getElementById('temp').value);
+var speed , b = parseFloat(document.getElementById('windSpeed').value);
+var wc = windchill ( temp , a , speed, b)
+document.getElementById ('output').innerHTML = wc;
+}
+function windChill (tempF,speedb) 	
+{
+var temp , a = Math.pow(speedb,.16);
+return result = (35.74 + (.6215 * tempF) - (35.75* temp, a) + (.4275 * tempF * temp, a)).toFixed(2) + " &#8457; ";
+}
