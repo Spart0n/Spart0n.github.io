83b2c14f2f840b85b7c9bff633a08f568e4910e0
Spart0n <avizza88@gmail.com>
2020-12-03 16:00:10 -0800

    test rental page add

---
+++ lesson 12/js/data.js
@@ -0,0 +1,26 @@
+const requestURL = 'data/data.json';
+fetch(requestURL)
+	.then(function (response) {
+	return response.json();
+	})
+.then(function (jsonObject) {
+console.table(jsonObject);  // temporary checking for valid response and data parsing
+const Rental = jsonObject['Rental'];
+for (let i = 0; i < Rental.length; i++ ) {
+let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let image = document.createElement('img');
+  h2.textContent = Rental[i].make + ' ' + Rental[i].model;
+        p1.textContent = "make: " + Rental[i].make + ' ' 
+        p2.textContent = "model: " + Rental[i].model;
+        image.setAttribute('src', Rental[i].image);
+        image.setAttribute('alt', Rental[i].make + ' ' + Rental[i].model + " - " + Rental[i].order);
+  card.appendChild(h2);
+        card.appendChild(p1);
+        card.appendChild(p2);
+        card.appendChild(image)
+document.querySelector('div.cards').appendChild(card);
+}	
+});
